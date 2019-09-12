586346a9016fc822bbac54b36cae9173263287f3
SwenCremers <55194529+SwenCremers@users.noreply.github.com>
2019-09-12 11:46:17 +0200

    Add files via upload

---
+++ GC%_calculate.py
@@ -0,0 +1,12 @@
+#reads a flat text file and uses the count and
+#len function to calculate the GC% and prints it
+sequentie = open("DictyosteliumDiscoideumMRNANoEnters.txt")
+sequentie = sequentie.read()
+
+aantal_g = sequentie.count("G")
+aantal_c = sequentie.count("C")
+lengte = len(sequentie)
+gc_percent = (aantal_g + aantal_c) / lengte * 100
+
+print("G:", aantal_g, "\n" "C:", aantal_c, "\n"  "lengte:"
+      , lengte,"\n"  "GC%:", gc_percent  )
