c97582d70339c007874b44e6b3c1f608700b7fc0
SwenCremers <55194529+SwenCremers@users.noreply.github.com>
2019-09-18 15:05:37 +0200

    Update GC%_calculate.py

--- GC%_calculate.py
+++ GC%_calculate.py
@@ -4,10 +4,10 @@
 sequentie = open("DictyosteliumDiscoideumMRNANoEnters.txt")# opens a txt file
 sequentie = sequentie.read()# reads the opened txt file
 
-aantal_g = sequentie.count("G")#counts number of G's in a string
-aantal_c = sequentie.count("C")#counts number of C's in a string
-lengte = len(sequentie)#creates an int using the predefined function len()
-gc_percent = (aantal_g + aantal_c) / lengte * 100#calculates gc percentage
+aantal_g = sequentie.count("G")# counts number of G's in a string
+aantal_c = sequentie.count("C")# counts number of C's in a string
+lengte = len(sequentie)# creates an int using the predefined function len()
+gc_percent = (aantal_g + aantal_c) / lengte * 100# calculates gc percentage
 
-print("G:", aantal_g, "\n" "C:", aantal_c, "\n"  "lengte:" #prints all calculations 
+print("G:", aantal_g, "\n" "C:", aantal_c, "\n"  "lengte:" 
       , lengte,"\n"  "GC%:", gc_percent  )
